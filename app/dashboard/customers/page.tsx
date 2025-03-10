27057e7afc7f523fc0d3c1faa266d6fbfcd353d7
Alice Cantele <alicecantele@gmail.com>
2025-03-10 13:32:43 -0300

    fix: suspense customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,8 @@
 import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
@@ -11,7 +13,9 @@ const customers = await fetchFilteredCustomers("");
 export default function Page() {
   return (
     <main>
-      <CustomersTable customers={customers} />
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
     </main>
   );
 }
