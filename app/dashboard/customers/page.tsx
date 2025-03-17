ac8b102bff13786b9040c40a771ff28050af4256
Alice Cantele <alicecantele@gmail.com>
2025-03-17 17:41:20 -0300

    fix: customers search bar and pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,21 +1,39 @@
-import { fetchFilteredCustomers } from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
 import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Metadata } from "next";
 import { Suspense } from "react";
+import { fetchCustomersPages } from "@/app/lib/data";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-const customers = await fetchFilteredCustomers("");
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
 
-export default function Page() {
   return (
     <main>
-      <Suspense fallback={<InvoicesTableSkeleton />}>
-        <CustomersTable customers={customers} />
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable query={query} currentPage={currentPage} />
       </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </main>
   );
 }
